c40608eb7750bbc1e452247b21f881f0ac854ffd
Frederick Burkley <burkley@gmail.com>
2018-10-31 20:39:11 -0400

    Cleanup.  Checking JFileChooser.SELECTED_FILES_CHANGED_PROPERTY in propertyChange() method.

--- src/main/java/org/fgb/fileOperations/xml/listeners/FileSelectionPropertyChangeListener.java
+++ src/main/java/org/fgb/fileOperations/xml/listeners/FileSelectionPropertyChangeListener.java
@@ -8,7 +8,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.swing.JFileChooser;
 
 
 public class FileSelectionPropertyChangeListener implements PropertyChangeListener {
@@ -25,32 +28,23 @@ public class FileSelectionPropertyChangeListener implements PropertyChangeListen
      * 
      */
     public void propertyChange(PropertyChangeEvent event) {
-	this.selectedFiles.clear();
-	Object oldValue = event.getOldValue();
-	Object newValue = event.getNewValue();
-	System.out.println(_className + ".propertyChange(): event.getPropertyName() = " + event.getPropertyName());
-//	System.out.println(_className + ".propertyChange(): event.getOldValue() = " + oldValue);
-	if (oldValue != null) {
-//	    System.out.println(_className + ".propertyChange(): event.getOldValue().getClass().getTypeName() = " + event.getOldValue().getClass().getTypeName());
-	    if (oldValue.getClass().isArray()) {
-//		System.out.println(_className + ".propertyChange(): oldValue is an Array.  Length = " + Array.getLength(oldValue));
-		for(int i=0; i<Array.getLength(oldValue); i++) {
-		    System.out.println(Array.get(oldValue, i).toString());
-		}
-	    }
-	}
-	System.out.println(_className + ".propertyChange(): event.getNewValue() = " + newValue);
-	if (newValue != null) {
-//	    System.out.println(_className + ".propertyChange(): event.getNewValue().getClass().getTypeName() = " + newValue.getClass().getTypeName());
-	    if (newValue.getClass().isArray()) {
-//		System.out.println(_className + ".propertyChange(): newValue is an Array.  Length = " + Array.getLength(newValue));
-		for(int i=0; i<Array.getLength(newValue); i++) {
-//		    System.out.println(Array.get(newValue, i).toString());
-		    if (Array.get(newValue, i) instanceof File) {
-			this.selectedFiles.add((File)Array.get(newValue, i));
+
+	if (JFileChooser.SELECTED_FILES_CHANGED_PROPERTY.equals(event.getPropertyName())) {
+	    this.selectedFiles.clear();
+	    Object newValue = event.getNewValue();
+//	    System.out.println(_className + ".propertyChange(): event.getPropertyName() = " + event.getPropertyName());
+//	    System.out.println(_className + ".propertyChange(): event.getNewValue() = " + newValue);
+	    if (newValue != null) {
+		if (newValue.getClass().isArray()) {
+		    for (int i = 0; i < Array.getLength(newValue); i++) {
+			_logger.logp(Level.FINE, _className, "propertyChange", Array.get(newValue, i).toString());
+			if (Array.get(newValue, i) instanceof File) {
+			    this.selectedFiles.add((File) Array.get(newValue, i));
+			}
 		    }
 		}
 	    }
+//	    System.out.println(_className + ".propertyChange():");
 	}
     }
 
