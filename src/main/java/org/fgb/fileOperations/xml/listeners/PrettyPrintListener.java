9e3f2904f9a120991dc7c3dc0fc66539ecc0e3d6
Frederick Burkley <burkley@gmail.com>
2018-12-23 08:24:39 -0500

    Reformatted.  No code change.

--- src/main/java/org/fgb/fileOperations/xml/listeners/PrettyPrintListener.java
+++ src/main/java/org/fgb/fileOperations/xml/listeners/PrettyPrintListener.java
@@ -6,32 +6,31 @@ import java.io.File;
 import java.util.logging.Logger;
 
 public class PrettyPrintListener implements ActionListener {
-    /**
-     * The name of this class.
-     */
-    private static final String _className = PrettyPrintListener.class.getName();
-    /**
-     * JDK logger.
-     */
-    private static final Logger _logger = Logger.getLogger(_className);
-    private final DefaultActionListener defaultListener;
+	/**
+	 * The name of this class.
+	 */
+	private static final String _className = PrettyPrintListener.class.getName();
+	/**
+	 * JDK logger.
+	 */
+	private static final Logger _logger = Logger.getLogger(_className);
+	private final DefaultActionListener defaultListener;
 
-    /**
-     * 
-     */
-    public PrettyPrintListener() {
-	this.defaultListener = new DefaultActionListener();
-    }
-
-
-    /**
-     * 
-     */
-    public void actionPerformed(ActionEvent event) {
-	this.defaultListener.actionPerformed(event);
-	for (File file : this.defaultListener.getSelectedFiles()) {
-	    System.out.println(_className + ".actionPerformed(): Selected file = " + file.getAbsolutePath());
+	/**
+	 * 
+	 */
+	public PrettyPrintListener() {
+		this.defaultListener = new DefaultActionListener();
 	}
-    }
+
+	/**
+	 * 
+	 */
+	public void actionPerformed(ActionEvent event) {
+		this.defaultListener.actionPerformed(event);
+		for (File file : this.defaultListener.getSelectedFiles()) {
+			System.out.println(_className + ".actionPerformed(): Selected file = " + file.getAbsolutePath());
+		}
+	}
 
 }
