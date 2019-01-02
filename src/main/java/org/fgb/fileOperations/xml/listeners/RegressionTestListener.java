fa595cdf4a287ea588b0853705d75b258a7657b4
Frederick Burkley <burkley@gmail.com>
2019-01-01 20:59:20 -0500

    Added a constructor that takes a JPanel.

--- src/main/java/org/fgb/fileOperations/xml/listeners/RegressionTestListener.java
+++ src/main/java/org/fgb/fileOperations/xml/listeners/RegressionTestListener.java
@@ -7,34 +7,52 @@ import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.util.logging.Logger;
 
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
 public class RegressionTestListener implements ActionListener {
-    /**
-     * The name of this class.
-     */
-    private static final String _className = RegressionTestListener.class.getName();
-    /**
-     * JDK logger.
-     */
-    private static final Logger _logger = Logger.getLogger(_className);
-    private final DefaultActionListener defaultListener;
+	/**
+	 * The name of this class.
+	 */
+	private static final String _className = RegressionTestListener.class.getName();
+	/**
+	 * JDK logger.
+	 */
+	private static final Logger _logger = Logger.getLogger(_className);
+	private final DefaultActionListener defaultListener;
+	/**
+	 * There is a single <i>results panel</i> for the application. This is the panel
+	 * where results of the various <i>work flows</i> will be published. The
+	 * reference to this panel is passed to this class as an argument to the
+	 * Constructor.
+	 */
+	private JComponent resultsPanel = new JPanel();
 
-
-    /**
-     * 
-     */
-    public RegressionTestListener() {
-	this.defaultListener = new DefaultActionListener();
-    }
-
-
-    /**
-     * 
-     */
-    public void actionPerformed(ActionEvent event) {
-	this.defaultListener.actionPerformed(event);
-	for (File file : this.defaultListener.getSelectedFiles()) {
-	    System.out.println(_className + ".actionPerformed(): Selected file = " + file.getAbsolutePath());
+	/**
+	 * 
+	 */
+	public RegressionTestListener() {
+		this.defaultListener = new DefaultActionListener();
+		System.out.println(_className + ".RegressionTestListener()...");
 	}
-    }
+
+	/**
+	 * 
+	 */
+	public RegressionTestListener(final JPanel panel) {
+		this();
+		System.out.println(_className + ".RegressionTestListener(JPanel j)...");
+		this.resultsPanel = panel;
+	}
+
+	/**
+	 * 
+	 */
+	public void actionPerformed(ActionEvent event) {
+		this.defaultListener.actionPerformed(event);
+		for (File file : this.defaultListener.getSelectedFiles()) {
+			System.out.println(_className + ".actionPerformed(): Selected file = " + file.getAbsolutePath());
+		}
+	}
 
 }
