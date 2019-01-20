b27d5a75a9bb54ba830d9c99b831df2984e256a8
Frederick Burkley <burkley@gmail.com>
2019-01-20 08:14:18 -0500

    Deleted inner class PrettyPrintWorker.  PrettyPrintWorker is now its own normal class.

--- src/main/java/org/fgb/fileOperations/xml/listeners/PrettyPrintListener.java
+++ src/main/java/org/fgb/fileOperations/xml/listeners/PrettyPrintListener.java
@@ -24,6 +24,9 @@ public class PrettyPrintListener implements ActionListener {
 	 * JDK logger.
 	 */
 	private static final Logger _logger = Logger.getLogger(_className);
+	/**
+	 * 
+	 */
 	private final DefaultActionListener defaultListener;
 	/**
 	 * There is a single <i>results panel</i> for the application. This is the panel
@@ -40,11 +43,6 @@ public class PrettyPrintListener implements ActionListener {
 	public PrettyPrintListener() {
 		this.defaultListener = new DefaultActionListener();
 		System.out.println(_className + ".PrettyPrintListener()...");
-//		try {
-//			throw new Exception("FGB Test PPL...");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//		}
 	}
 
 	/**
@@ -72,75 +70,6 @@ public class PrettyPrintListener implements ActionListener {
 		}
 		System.out.println(
 				_className + ".actionPerformed(): isEventDispatchThread = " + SwingUtilities.isEventDispatchThread());
-
-		PrettyPrintSwingWorker worker = new PrettyPrintSwingWorker();
-		worker.execute();
-
 		_logger.exiting(_className, "actionPerformed");
-	}
-
-
-	/**
-	 * SwingWorker is a generic class, with two type parameters.  The first type parameter specifies a return type for
-	 * doInBackground, and also for the get method, which is invoked by other threads to retrieve the object returned
-	 * by doInBackground.
-	 * SwingWorker's second type parameter specifies a type for interim results returned while the background task is
-	 * still active.  If the SwingWorker doesn't return interim results, Void can be used as a placeholder.
-	 * 
-	 * @author Burkley
-	 *
-	 */
-	class PrettyPrintSwingWorker extends SwingWorker<String, String> {
-
-		@Override
-		protected String doInBackground() throws Exception {
-			StringBuilder msg = new StringBuilder();
-			int totalSleepTime = 15000;
-			int sleptSoFar = 0;
-			int sleepTime = 1000;
-			System.out.println(this.getClass().getName() + ".doInBackground(): isEventDispatchThread = "
-					+ SwingUtilities.isEventDispatchThread());
-			try {
-				while (sleptSoFar < totalSleepTime) {
-					msg.append("Going to sleep for ").append(sleepTime).append(" milliseconds.");
-//					System.out.println(this.getClass().getName() + ".doInBackground(): " + msg.toString());
-					publish(msg.toString());
-					msg.delete(0, msg.length());
-					Thread.sleep(sleepTime);
-					sleptSoFar += sleepTime;
-					msg.append("  Wake up.  Have slept for a total of ").append(sleptSoFar).append(" milliseconds.\n");
-//					System.out.println(this.getClass().getName() + ".doInBackground(): " + msg.toString());
-					publish(msg.toString());
-					msg.delete(0, msg.length());
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (Throwable th) {
-				th.printStackTrace();
-			}
-			return "All Done!";
-		}
-
-
-		@Override
-		protected void process(List<String> chunks) {
-			super.process(chunks);
-			System.out.println(this.getClass().getName() + ".process(): isEventDispatchThread = " + SwingUtilities.isEventDispatchThread());
-			for (String chunk : chunks) {
-				PrettyPrintListener.this.resultsTextArea.append(chunk);
-				PrettyPrintListener.this.resultsTextArea.setCaretPosition(PrettyPrintListener.this.resultsTextArea.getDocument().getLength());				
-			}
-		}
-
-
-		@Override
-		protected void done() {
-			super.done();
-			System.out.println(this.getClass().getName() + ".done(): isEventDispatchThread = "
-					+ SwingUtilities.isEventDispatchThread());
-			System.out.println(this.getClass().getName() + ".done(): Done...");
-		}
-
 	}
 }
