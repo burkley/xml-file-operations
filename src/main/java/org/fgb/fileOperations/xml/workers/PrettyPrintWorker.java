585f8c117906a3d42daa3c02c82cfef6e84a42dc
Frederick Burkley <burkley@gmail.com>
2019-01-20 08:28:34 -0500

    Cooperating with cancellation request.  Passing list of selected files to the constructor.

--- src/main/java/org/fgb/fileOperations/xml/workers/PrettyPrintWorker.java
+++ src/main/java/org/fgb/fileOperations/xml/workers/PrettyPrintWorker.java
@@ -1,9 +1,8 @@
 package org.fgb.fileOperations.xml.workers;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -14,7 +13,6 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 
 import org.fgb.fileOperations.xml.listeners.DefaultActionListener;
-import org.fgb.fileOperations.xml.listeners.PrettyPrintListener;
 import org.fgb.fileOperations.xml.utilities.SwingUtils;
 
 /**
@@ -27,19 +25,15 @@ import org.fgb.fileOperations.xml.utilities.SwingUtils;
  * @author Burkley
  *
  */
-public class PrettyPrintWorker extends SwingWorker<String, String> implements ActionListener {
+public class PrettyPrintWorker extends SwingWorker<String, String> {
 	/**
 	 * The name of this class.
 	 */
-	private static final String _className = PrettyPrintListener.class.getName();
+	private static final String _className = PrettyPrintWorker.class.getName();
 	/**
 	 * JDK logger.
 	 */
 	private static final Logger _logger = Logger.getLogger(_className);
-	/**
-	 * 
-	 */
-	private final DefaultActionListener defaultListener;
 	/**
 	 * There is a single <i>results panel</i> for the application. This is the panel
 	 * where results of the various <i>work flows</i> will be published. The
@@ -47,43 +41,63 @@ public class PrettyPrintWorker extends SwingWorker<String, String> implements Ac
 	 * Constructor.
 	 */
 	private JComponent resultsPanel = new JPanel();
+	/**
+	 * A list of the files that are currently selected from the <code>JFileChooser</code>.
+	 * This is the list of files that will be processed by this class.
+	 */
+	private List<File> selectedFiles;
+
 	private JTextArea resultsTextArea = null;
 
-	public PrettyPrintWorker(final JComponent panel) {
-		this.defaultListener = new DefaultActionListener();
-		System.out.println(_className + ".PrettyPrintListener(JPanel j): panel.getName() = " + panel.getName());
+	public PrettyPrintWorker(final JComponent panel, List<File> files) {
+		System.out.println(_className + ".PrettyPrintWorker(JPanel j): panel.getName() = " + panel.getName());
 		this.resultsPanel = panel;
 		Component c;
 		if ((c = SwingUtils.findComponentByName(panel, "ResultsTextArea")) != null) {
 			this.resultsTextArea = (JTextArea) c;
-			System.out.println(_className + ".PrettyPrintListener(): c.getName() = " + c.getName());
+//			System.out.println(_className + ".PrettyPrintWorker(): c.getName() = " + c.getName());
 		}
+		this.selectedFiles = new ArrayList<File>(files);
 	}
 
 
 	@Override
 	protected String doInBackground() throws Exception {
 		StringBuilder msg = new StringBuilder();
-		int totalSleepTime = 15000;
+		int totalSleepTime = 10000;
 		int sleptSoFar = 0;
 		int sleepTime = 1000;
 		System.out.println(this.getClass().getName() + ".doInBackground(): isEventDispatchThread = "
 				+ SwingUtilities.isEventDispatchThread());
+		msg.append("The list of selected files: ");
+		msg.append(this.selectedFiles.toString());
+		msg.append("\n");
+		this.publish(msg.toString());
+		msg.delete(0, msg.length());
 		try {
 			while (sleptSoFar < totalSleepTime) {
 				msg.append("Going to sleep for ").append(sleepTime).append(" milliseconds.");
 //				System.out.println(this.getClass().getName() + ".doInBackground(): " + msg.toString());
-				publish(msg.toString());
+				this.publish(msg.toString());
 				msg.delete(0, msg.length());
 				Thread.sleep(sleepTime);
 				sleptSoFar += sleepTime;
 				msg.append("  Wake up.  Have slept for a total of ").append(sleptSoFar).append(" milliseconds.\n");
 //				System.out.println(this.getClass().getName() + ".doInBackground(): " + msg.toString());
-				publish(msg.toString());
+				this.publish(msg.toString());
 				msg.delete(0, msg.length());
+				if (this.isCancelled()) {
+					msg.append("\n");
+					msg.append("Operation cancelled...");
+					this.publish(msg.toString());
+					break;
+				}
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			msg.append("\n");
+			msg.append("Operation cancelled...");
+			this.publish(msg.toString());
+			msg.delete(0, msg.length());
 			e.printStackTrace();
 		} catch (Throwable th) {
 			th.printStackTrace();
@@ -106,25 +120,9 @@ public class PrettyPrintWorker extends SwingWorker<String, String> implements Ac
 	@Override
 	protected void done() {
 		super.done();
-		System.out.println(this.getClass().getName() + ".done(): isEventDispatchThread = "
-				+ SwingUtilities.isEventDispatchThread());
-		System.out.println(this.getClass().getName() + ".done(): Done...");
+//		System.out.println(this.getClass().getName() + ".done(): isEventDispatchThread = "
+//				+ SwingUtilities.isEventDispatchThread());
+//		System.out.println(this.getClass().getName() + ".done(): Done...");
+		this.selectedFiles.clear();
 	}
-
-
-	/**
-	 * 
-	 */
-	public void actionPerformed(ActionEvent event) {
-		_logger.entering(_className, "actionPerformed");
-		this.defaultListener.actionPerformed(event);
-		for (File file : this.defaultListener.getSelectedFiles()) {
-			System.out.println(_className + ".actionPerformed(): Selected file = " + file.getAbsolutePath());
-		}
-		System.out.println(
-				_className + ".actionPerformed(): isEventDispatchThread = " + SwingUtilities.isEventDispatchThread());
-
-		_logger.exiting(_className, "actionPerformed");
-	}
-
 }
